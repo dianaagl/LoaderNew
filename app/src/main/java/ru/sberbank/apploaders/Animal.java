ab1aeaf92fd4b61bf2ddb7474f7350e0e7ff2dc2
dianaagl <aglyamutdinova.d@yandex.ru>
2017-05-23 15:44:32 +0300

    equals hashcode

--- app/src/main/java/ru/sberbank/apploaders/Animal.java
+++ app/src/main/java/ru/sberbank/apploaders/Animal.java
@@ -10,6 +10,28 @@ public class Animal {
     private int height;
     private String name;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Animal)) return false;
+
+        Animal animal = (Animal) o;
+
+        if (weight != animal.weight) return false;
+        if (height != animal.height) return false;
+        if (specie != null ? !specie.equals(animal.specie) : animal.specie != null) return false;
+        return name != null ? name.equals(animal.name) : animal.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = specie != null ? specie.hashCode() : 0;
+        result = 31 * result + weight;
+        result = 31 * result + height;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
     public Animal(String specie, int mass, int height, String name) {
         this.specie = specie;
         this.weight = mass;
